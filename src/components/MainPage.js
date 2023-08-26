fe51052d4f1567be2a0fc4e2f053d1b836e64111
Seunggyu <seunggyul14@icloud.com>
2023-08-26 12:19:43 -0700

    boxes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const MainPage = () => {
     return (
-        <div className='intro'>
-             I am a self-proclaimed software engineer in search of opportunities to embark on my journey in the field of computer science.
+        <div>
+            <img src='https://i.pinimg.com/236x/17/d7/a2/17d7a2d33bc4c3ac756cb8311451bb00.jpg' alt="pfp" className='pfp'/>
+            <div className='intro'>
+                I am a self-proclaimed software engineer in search of opportunities to embark on my journey in the field of computer science.
+            </div>
+
+            <div className='boxes'>
+                <div className='about-me'></div>
+                <div className='my-work'></div>
+            </div>
         </div>
     );
 }
