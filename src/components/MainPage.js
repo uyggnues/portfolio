7907ea64d0983c68f3beac85709154163e3cb66b
Seunggyu <seunggyul14@icloud.com>
2023-08-26 12:00:12 -0700

    started

---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div className='intro'>
+             I am a self-proclaimed software engineer in search of opportunities to embark on my journey in the field of computer science.
+        </div>
+    );
+}
+
+export default MainPage;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const Navbar = () => {
 
     
 
     return (
-        <div id="navbar-animmenu">
-            <ul class="show-dropdown main-navbar">
-                <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
-                <p>
-                    <a href="/">Seunggyu Lee</a>
-                </p>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
-            </ul>
+        <div id="navbar">
+                <a href="/">Seunggyu Lee</a>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/about">About</a>
+            </li>
+            <li>
+                <a href="/contact">Contact</a>
+            </li>
         </div>
     );
 }
