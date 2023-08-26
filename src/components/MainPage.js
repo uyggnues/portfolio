2b7e74f7b1d24849e0562a7e3ff261fc2927a6a5
Seunggyu <seunggyul14@icloud.com>
2023-08-26 13:31:46 -0700

    heroku test

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,7 +5,7 @@ const MainPage = () => {
         <div>
             <img src='https://i.pinimg.com/236x/17/d7/a2/17d7a2d33bc4c3ac756cb8311451bb00.jpg' alt="pfp" className='pfp'/>
             <div className='intro'>
-                I am a self-proclaimed software engineer in search of opportunities to embark on my journey in the field of computer science.
+                I am a software engineer in search of opportunities to embark on my journey in the field of computer science.
             </div>
 
             <div className='boxes'>
